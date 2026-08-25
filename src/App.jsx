a7fc2ce5a5b008c67e467507f2023c1294a9ee1c
nireesha <narendulanireesha@gmail.com>
2026-08-24 21:50:52 -0500

    updated Index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar.jsx";
+import { Footer } from "./components/Footer.jsx";
 import { GardenManager } from "./components/GardenManager.jsx";
+import { HomeDashboard } from "./components/HomeDashboard.jsx";
+import { CarePlanner } from "./components/CarePlanner.jsx";
+import { MowingHelper } from "./components/MowingHelper.jsx";
+import { About } from "./components/About.jsx";
 
 export default function App() {
   return (
-    <main>
-      <h1>LawnPulse</h1>
-      <GardenManager />
-    </main>
+    <div className="app-layout">
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomeDashboard />} />
+          <Route path="/inventory" element={<GardenManager />} />
+          <Route path="/planner" element={<CarePlanner />} />
+          <Route path="/mowing" element={<MowingHelper />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
