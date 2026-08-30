7a0183ef2878e7f1d610270b39be992d46d970f6
nireesha <narendulanireesha@gmail.com>
2026-08-30 17:46:19 -0500

    updated with css classnames

--- src/components/GardenList.jsx
+++ src/components/GardenList.jsx
@@ -3,12 +3,14 @@ import { GardenItem } from "./GardenItem.jsx";
 export function GardenList({ items, onDeleteItem, onStartEdit }) {
   if (items.length === 0) {
     return (
-      <p>No garden items found. Add your first item using the form above!</p>
+      <p className="empty-message">
+        No garden items found. Add your first item using the form above!
+      </p>
     );
   }
 
   return (
-    <div>
+    <div className="garden-list">
       {items.map((item) => (
         <GardenItem
           key={item.id}
