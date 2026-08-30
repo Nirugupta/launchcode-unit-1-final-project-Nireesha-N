39b6ac6d57dc00af62fca4764e3d7a9e38a875ce
nireesha <narendulanireesha@gmail.com>
2026-08-30 17:40:09 -0500

    udpated props in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import { initialGardenItems } from "./testdata/mockLawnData.js";
 import { Navbar } from "./components/Navbar.jsx";
 import { Footer } from "./components/Footer.jsx";
 import { GardenManager } from "./components/GardenManager.jsx";
 import { HomeDashboard } from "./components/HomeDashboard.jsx";
 import { CarePlanner } from "./components/CarePlanner.jsx";
-import { MowingHelper } from "./components/MowingHelper.jsx";
 import { About } from "./components/About.jsx";
+import "./App.css";
 
 export default function App() {
+  const [items, setItems] = useState(initialGardenItems);
+
   return (
     <div className="app-layout">
       <Navbar />
       <main>
         <Routes>
-          <Route path="/" element={<HomeDashboard />} />
-          <Route path="/inventory" element={<GardenManager />} />
+          <Route
+            path="/"
+            element={<HomeDashboard inventoryCount={items.length} />}
+          />
+          <Route
+            path="/inventory"
+            element={<GardenManager items={items} setItems={setItems} />}
+          />
           <Route path="/planner" element={<CarePlanner />} />
-          <Route path="/mowing" element={<MowingHelper />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </main>
